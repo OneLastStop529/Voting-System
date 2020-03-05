7de211602f67e959aa2f6dd2a407a97fea025113
onelaststop529 <yize.hu@wsu.edu>
2020-03-05 15:07:01 -0800

    Spring-boot Setup

---
+++ src/main/groovy/com/example/VotingSystem/VotingSystemApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.VotingSystem
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VotingSystemApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(VotingSystemApplication, args)
+	}
+
+}
